28e9693a3e0ae4ebec2be2ba5ce7130e40000df7
Ed Baker <edwbaker@gmail.com>
2020-06-17 17:01:56 +0100

    Update rpa-dac-profile.d.sh

--- inst/rpa-dac-profile.d.sh
+++ inst/rpa-dac-profile.d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo alsactl restore -f /rpa-dac-asound.state
-sudo rm ./rpa-dac-asound.state
+sudo rm /rpa-dac-asound.state
 
 # Delete me
-sudo rm /etc/profile.d/rpa-dac-profile.d
+sudo rm /etc/profile.d/rpa-dac-profile.d.sh
