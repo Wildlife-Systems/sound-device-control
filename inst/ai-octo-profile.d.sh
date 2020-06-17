dbcc10775b16dc06294eda200ef0113ca5b19daf
Ed Baker <edwbaker@gmail.com>
2020-06-17 17:56:09 +0100

    Create ai-octo-profile.d.sh

---
+++ inst/ai-octo-profile.d.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DEVTEST=`aplay -l | grep -c "no soundcards found"`
+if [ "$DEVTEST" -eq "1" ]; then
+  sudo modprobe -r snd_soc_audioinjector_octo_soundcard
+  sudo modprobe -r snd_soc_cs42xx8_i2c
+  sudo modprobe -r snd_soc_cs42xx8
+  sudo modprobe snd_soc_cs42xx8
+  sudo modprobe snd_soc_cs42xx8_i2c
+  sudo modprobe snd_soc_audioinjector_octo_soundcard
+fi
